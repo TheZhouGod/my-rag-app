5b9027e8f0541aea2ff920c069a79cfa23bfb1a1
TheZhouGod <113572797+TheZhouGod@users.noreply.github.com>
2026-04-28 13:34:55 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from langchain_text_splitters import (
 )
 from langchain_community.embeddings import ZhipuAIEmbeddings
 from langchain_chroma import Chroma
-from langchain_community.retrievers import BM25Retriever
-from langchain.retrievers import EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever, EnsembleRetriever
 from zhipuai import ZhipuAI
 
 # ================= 🔧 基础配置 =================
